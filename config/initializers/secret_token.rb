53dd9c7dc4564de457a7389ea42700ce9516f393
Matt Banasiak <mattbanasiak@hotmail.com>
2013-12-29 14:24:54 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lovecleaning::Application.config.secret_token = '1f726b1fcac85b466cca684c575e264a669db49a09c005b820a3dd44e94182732159a201d0df6d015b0ba1838e60a3522360bef8e64af046bc943c87e74694fa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lovecleaning::Application.config.session_store :cookie_store, key: '_lovecleaning_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lovecleaning::Application.config.session_store :active_record_store
